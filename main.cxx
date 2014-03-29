89d2a92c3cff2ef6ac821dd8164abf352d3f108a
Michael Pogoda <mi.pogoda@samsung.com>
2014-03-29 20:01:03 +0200

    Add example python code to main

--- main.cxx
+++ main.cxx
@@ -6,14 +6,44 @@
 
 int main( int /* argc */, char* /* argv */[] )
 {
-    std::string str("    , + - \n    \n        =\n\n\n");
+    std::string str("class someclass:\n"
+                    "    def foo( self, x ):\n"
+                    "        if x == 5:\n"
+                    "            print( \"foo\" )\n"
+                    "        else:\n"
+                    "            print( \"bar\" )\n"
+                    "\n"
+                    "    def bar( self, a, b ):\n"
+                    "        c = b - a\n"
+                    "        return c\n"
+                    "\n"
+                    "class someotherclass:\n"
+                    "    def foo( self, a, b):\n"
+                    "        c = someclass()\n"
+                    "        z = c.bar( a, b )\n"
+                    "        c.foo( z )\n"
+                    "\n"
+                    "\n"
+                    "class main:\n"
+                    "    def run( self ):\n"
+                    "        m = someotherclass()\n"
+                    "        m.foo( 5, 10 )\n"
+                    "        m.foo( 3, 6 )\n"
+                    "\n"
+                    "\n"
+                    "main().run()\n"
+                    );
+
+
+
+
     std::cin.unsetf( std::ios::skipws );
 
     sap::Lexems lexems = sap::parse( str );
 
     std::cout << "\n\n\n";
     for (const auto& lexem : lexems )
-        std::cout << lexem << "; ";
+        std::cout << lexem << '\n';
 
     std::cout << '\n';
 
